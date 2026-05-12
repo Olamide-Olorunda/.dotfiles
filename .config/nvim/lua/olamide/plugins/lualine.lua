08ba54259eeb9fcf826f533a33019500d115f753
Olamide-Olorunda <olamideolorunda268@gmail.com>
2026-05-12 12:35:28 +0100

    lualine error fixed

--- .config/nvim/lua/olamide/plugins/lualine.lua
+++ .config/nvim/lua/olamide/plugins/lualine.lua
@@ -1,16 +1,15 @@
 return {
     "nvim-lualine/lualine.nvim",
-    -- Web Devicons gives the status bar those little folder and language logos
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         local custom_gruvbox = require('lualine.themes.gruvbox-material')
 
-        custom_gruvbox.normal.c.bg = 'NONE'
-        custom_gruvbox.insert.c.bg = 'NONE'
-        custom_gruvbox.visual.c.bg = 'NONE'
-        custom_gruvbox.replace.c.bg = 'NONE'
-        custom_gruvbox.command.c.bg = 'NONE'
-
+        local modes = { 'normal', 'insert', 'visual', 'replace', 'command' }
+        for _, mode in ipairs(modes) do
+            if custom_gruvbox[mode] and custom_gruvbox[mode].c then
+                custom_gruvbox[mode].c.bg = 'NONE'
+            end
+        end
 
         require('lualine').setup({
             options = {
@@ -24,7 +23,6 @@ return {
                 lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
                 lualine_c = { 'filename' },
-                -- Shows exactly what language server is currently running
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
