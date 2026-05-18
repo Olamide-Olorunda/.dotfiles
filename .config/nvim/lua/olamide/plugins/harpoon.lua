038dbf3c336d9e997aa61187b70880c7b7160ec4
Olamide-Olorunda <olamideolorunda268@gmail.com>
2026-05-18 15:35:33 +0100

    nvim config

--- .config/nvim/lua/olamide/plugins/harpoon.lua
+++ .config/nvim/lua/olamide/plugins/harpoon.lua
@@ -1,23 +1,27 @@
 return {
-    "ThePrimeagen/harpoon",
-    dependencies = {
-        "nvim-lua/plenary.nvim", -- Plenary is already installed via Telescope, but it's good practice to list it
-    },
-    config = function()
-        local mark = require("harpoon.mark")
-        local ui = require("harpoon.ui")
+  "ThePrimeagen/harpoon",
+  dependencies = {
+    "nvim-lua/plenary.nvim", -- Plenary is already installed via Telescope, but it's good practice to list it
+  },
+  config = function()
+    local mark = require("harpoon.mark")
+    local ui = require("harpoon.ui")
 
-        -- Space + a: Append the current file to Harpoon
-        vim.keymap.set("n", "<leader>a", mark.add_file)
-        
-        -- Ctrl + e: View all your currently Harpooned files
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+    settings = {
+      save_on_toggle = true,
+      sync_on_ui_close = true,
+    }
 
-        -- Ctrl + h/t/n/s: Jump directly to file 1, 2, 3, or 4 on the Harpoon list
-        -- (These are right on your home row for instant jumping)
-        vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-        vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-        vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-        vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-    end
+    vim.keymap.set("n", "<leader>a", mark.add_file)
+
+    -- Ctrl + e: View all your currently Harpooned files
+    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+    -- Ctrl + h/t/n/s: Jump directly to file 1, 2, 3, or 4 on the Harpoon list
+    -- (These are right on your home row for instant jumping)
+    vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+    vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+    vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+    vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+  end
 }
