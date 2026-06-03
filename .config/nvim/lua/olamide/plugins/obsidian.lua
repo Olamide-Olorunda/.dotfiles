f215740737c890175a336a727e8f696497b95c22
Olamide-Olorunda <olamideolorunda268@gmail.com>
2026-06-03 09:34:19 +0100

    i am batman

--- .config/nvim/lua/olamide/plugins/obsidian.lua
+++ .config/nvim/lua/olamide/plugins/obsidian.lua
@@ -12,25 +12,24 @@ return {
         name = "main",
         path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/daini do nō",
       },
-      -- Where to put new notes created from Neovim
-      new_notes_location = "current_dir",
-
-      completion = {
-        nvim_cmp = true,
-        min_chars = 2,
-      },
-      -- Customize how frontmatter is generated for new notes
-      note_frontmatter_func = function(note)
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
-        end
-        return out
-      end,
     },
-  }
 
+    new_notes_location = "current_dir",
+
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+    -- Customize how frontmatter is generated for new notes
+    note_frontmatter_func = function(note)
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+      return out
+    end,
+  },
 }
