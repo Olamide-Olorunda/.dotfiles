af4a1c11b573a3d54aec9571d045cdc4acee715d
Olamide-Olorunda <olamideolorunda268@gmail.com>
2026-06-13 19:33:25 +0100

    i am batman

--- .config/nvim/lua/olamide/plugins/lsp.lua
+++ .config/nvim/lua/olamide/plugins/lsp.lua
@@ -40,11 +40,26 @@ return {
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
     cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
       mapping = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
+      }),
+
+      sources = cmp.config,
+      sources({
+        { name = 'lazydev', group_index = 0 },
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+      }, {
+        { name = 'buffer' },
+        { name = 'path' },
       })
     })
   end
