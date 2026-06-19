f9338ee4327e6575785fbbcb5700d8cf9839ebf6
Olamide-Olorunda <olamideolorunda268@gmail.com>
2026-06-19 14:01:03 +0100

    i am batman

---
+++ .config/nvim/lua/olamide/plugins/oled_mono.lua
@@ -0,0 +1,75 @@
+return {
+  "oled_mono",
+  dir = "",
+  lazy = false,
+  enabled = false,
+  priority = 1000,
+  config = function()
+    vim.cmd("hi clear")
+    if vim.fn.exists("syntax_on") == 1 then
+      vim.cmd("syntax reset")
+    end
+
+    vim.o.background = "dark"
+    vim.o.termguicolors = true
+    vim.g.colors_name = "oled_mono"
+
+    local colors = {
+      bg = "NONE",
+      fg = "#e0e0e0",
+      white = "#ffffff",
+      grey = "#888888",
+      dark_grey = "#333333"
+    }
+
+    local highlights = {
+      Normal = { fg = colors.fg, bg = colors.bg },
+      NormalFloat = { fg = colors.fg, bg = colors.bg },
+      LineNr = { fg = colors.dark_grey, bg = colors.bg },
+      CursorLine = { bg = colors.dark_grey },
+      CursorLineNr = { fg = colors.white, bg = colors.bg, bold = true },
+      VertSplit = { fg = colors.dark_grey, bg = colors.bg },
+      SignColumn = { bg = colors.bg },
+      Comment = { fg = colors.grey, italic = true },
+      String = { fg = colors.grey },
+      Number = { fg = colors.white },
+      Boolean = { fg = colors.white, bold = true },
+      Keyword = { fg = colors.white, bold = true },
+      Statement = { fg = colors.white, bold = true },
+      Conditional = { fg = colors.white, bold = true },
+      Function = { fg = colors.white },
+      Identifier = { fg = colors.fg },
+      Constant = { fg = colors.white, bold = true },
+      Type = { fg = colors.white, bold = true },
+      Operator = { fg = colors.grey },
+
+      Pmenu = { fg = colors.fg, bg = colors.dark_grey },
+      PmenuSel = { fg = colors.bg, bg = colors.white },
+      Search = { fg = colors.bg, bg = colors.white },
+      IncSearch = { fg = colors.bg, bg = colors.white },
+      MatchParen = { fg = colors.white, bg = colors.dark_grey, bold = true },
+    }
+
+    for group, styles in pairs(highlights) do
+      vim.api.nvim_set_hl(0, group, styles)
+    end
+
+    local links = {
+      ["@variable"] = "Identifier",
+      ["@function"] = "Function",
+      ["@keyword"] = "Keyword",
+      ["@string"] = "String",
+      ["@comment"] = "Comment",
+      ["@type"] = "Type",
+      ["@operator"] = "Operator",
+      ["@number"] = "Number",
+      ["@boolean"] = "Boolean",
+      ["@property"] = "Identifier",
+      ["@parameter"] = "Identifier",
+    }
+
+    for newgroup, oldgroup in pairs(links) do
+      vim.api.nvim_set_hl(0, newgroup, { link = oldgroup })
+    end
+  end
+}
