1cc17edaae139f0b92dcd99318c305b9dc8116e5
Olamide-Olorunda <olamideolorunda268@gmail.com>
2026-06-19 14:01:29 +0100

    i am batman

--- .config/nvim/lua/olamide/plugins/gruvbox.lua
+++ .config/nvim/lua/olamide/plugins/gruvbox.lua
@@ -1,11 +1,12 @@
 return {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.g.gruvbox_material_background = 'hard'
-        vim.g.gruvbox_material_transparent_background = 1
-        vim.g.gruvbox_material_foreground = 'material'
-        vim.cmd.colorscheme('gruvbox-material')
-    end,
+  "sainnhe/gruvbox-material",
+  lazy = false,
+  enabled = false,
+  priority = 1000,
+  config = function()
+    vim.g.gruvbox_material_background = 'hard'
+    vim.g.gruvbox_material_transparent_background = 1
+    vim.g.gruvbox_material_foreground = 'material'
+    vim.cmd.colorscheme('gruvbox-material')
+  end,
 }
