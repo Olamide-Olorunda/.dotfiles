f3fedee7a15d6f2d37feb307eb6a1b7d5424e66f
Olamide-Olorunda <olamideolorunda268@gmail.com>
2026-06-20 20:32:40 +0100

    i am batman

--- .config/nvim/lua/olamide/remap.lua
+++ .config/nvim/lua/olamide/remap.lua
@@ -95,3 +95,10 @@ vim.keymap.set("n", "<leader>r", function()
 end, { desc = "Run current file" })
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
