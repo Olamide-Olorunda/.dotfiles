82be641e20dcf63b0d6ee4720ba0f0ef418ddd74
Olamide-Olorunda <olamideolorunda268@gmail.com>
2026-06-24 12:57:52 +0100

    i am batman

--- .config/nvim/lua/olamide/plugins/telescope.lua
+++ .config/nvim/lua/olamide/plugins/telescope.lua
@@ -1,25 +1,43 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require('telescope.builtin')
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-ui-select.nvim',
+  },
+  config = function()
+    local telescope = require('telescope')
+    local builtin = require('telescope.builtin')
 
-        -- Search for files by name
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        -- Search for files tracked by git
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        -- Search for specific text in files using grep
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        -- help page: see all available telescope commands
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-        -- to search for folders only
-        vim.keymap.set('n', '<leader>fd', function()
-            require('telescope.builtin').find_files({
-                prompt_title = "Browse Folders",
-                find_command = { "fd", "--type", "d", "--hidden", "--exclude", ".git" }
-            })
-        end, { desc = "Telescope: Search for folders only" })
-    end
+    telescope.setup({
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown({})
+        }
+      }
+    })
+
+    -- Safely load the extensions
+    pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'ui-select')
+
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Telescope: Find files" })
+    vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = "Telescope: Git files" })
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end, { desc = "Telescope: Grep string" })
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, { desc = "Telescope: Help tags" })
+    vim.keymap.set('n', '<leader>fd', function()
+      builtin.find_files({
+        prompt_title = "Browse Folders",
+        find_command = { "fd", "--type", "d", "--hidden", "--exclude", ".git" }
+      })
+    end, { desc = "Telescope: Search for folders only" })
+
+    -- Added for C/C++/Go Workflows
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Telescope: Live grep across project' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Telescope: Grep word under cursor' })
+    vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = 'Telescope: Find open buffers' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Telescope: Workspace diagnostics' })
+  end
 }
